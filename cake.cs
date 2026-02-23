6bcf0b43239931d153392d79833d698f80368d8c
Mattias Karlsson <mattias@devlead.se>
2026-02-23 23:14:07 +0100

    Add DPI package to InstallTools in cake.cs

--- cake.cs
+++ cake.cs
@@ -8,5 +8,6 @@ InstallTool("dotnet:?package=Cake.Tool&version=6.0.0");
 
 InstallTools(
     "dotnet:https://api.nuget.org/v3/index.json?package=GitVersion.Tool&version=6.6.0",
-    "dotnet:https://api.nuget.org/v3/index.json?package=GitReleaseManager.Tool&version=0.20.0"
+    "dotnet:https://api.nuget.org/v3/index.json?package=GitReleaseManager.Tool&version=0.20.0",
+    "dotnet:?package=DPI&version=2025.12.17.349"
 );

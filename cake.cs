7252c5662f2af87aa8cd7e1ade4926fa8e25bc4a
Mattias Karlsson <1647294+devlead@users.noreply.github.com>
2026-02-23 22:17:02 +0000

    Update dependency DPI to v2026

--- cake.cs
+++ cake.cs
@@ -9,5 +9,5 @@ InstallTool("dotnet:?package=Cake.Tool&version=6.0.0");
 InstallTools(
     "dotnet:https://api.nuget.org/v3/index.json?package=GitVersion.Tool&version=6.6.0",
     "dotnet:https://api.nuget.org/v3/index.json?package=GitReleaseManager.Tool&version=0.20.0",
-    "dotnet:?package=DPI&version=2025.12.17.349"
+    "dotnet:?package=DPI&version=2026.2.11.364"
 );
